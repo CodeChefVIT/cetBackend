87898598eab8ca54f3a45c5504a6f9553052930b
jugaldb <jugalbhatt3@gmail.com>
2020-12-17 13:36:34 +0530

    email sent done

--- api/config/studentGoogleAuth.js
+++ api/config/studentGoogleAuth.js
@@ -1,6 +1,7 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const Student = require("../models/student.model");
+const sgMail = require("@sendgrid/mail");
 const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
 require("dotenv").config();
@@ -28,7 +29,7 @@ passport.use(
     },
     async (accessToken, refreshToken, profile, done) => {
       const emailRegex = /[A-Za-z]+.[A-Za-z]*20[0-9]{2}[A-Za-z]*@vitstudent.ac.in/;
-      if(profile && !profile.emails[0].value.match(emailRegex)) {
+      if (profile && !profile.emails[0].value.match(emailRegex)) {
         console.log('failed')
         done(new Error("Invalid host domain"));
       }
@@ -66,6 +67,12 @@ passport.use(
                 });
             });
           } else {
+            const emailVerificationCode = Math.floor(
+              100000 + Math.random() * 900000
+            );
+  
+            const emailVerificationCodeExpires =
+              new Date().getTime() + 20 * 60 * 1000;
             // if not, create user in our db
             new Student({
               _id: new mongoose.Types.ObjectId(),
@@ -73,35 +80,60 @@ passport.use(
               name: profile.displayName,
               email: profile.emails[0].value,
               isEmailVerified: false,
+              emailVerificationCode,
+              emailVerificationCodeExpires,
             })
               .save()
-              .then((newUser) => {
-                console.log(newUser);
-                const token = jwt.sign(
-                  {
-                    userType: newUser.userType,
-                    userId: newUser._id,
-                    email: newUser.email,
-                    name: newUser.name,
-                    isEmailVerified: newUser.isEmailVerified,
+              .then(async (newUser) => {
+                const msg = {
+                  to: profile.emails[0].value,
+                  from: {
+                    email: process.env.SENDGRID_EMAIL,
+                    name: "CodeChef-VIT",
                   },
-                  process.env.JWT_SECRET,
-                  {
-                    expiresIn: "1d",
-                  }
-                );
-                Student.findById(newUser._id).then((result7) => {
-                  result7.token = token;
-                  result7
-                    .save()
-                    .then((user) => {
-                      console.log(user);
-                      done(null, user);
-                    })
-                    .catch((err) => {
-                      console.log(err);
+                  subject: `Common Entry Test - Email Verification`,
+                  text: `Use the following code to verify your email: ${emailVerificationCode}`,
+                  // html: EmailTemplates.tracker(
+                  //   users[i].name,
+                  //   companyArr[k].companyName,
+                  //   status
+                  // ),
+                };
+
+                await sgMail
+                  .send(msg)
+                  .then(async () => {
+                    console.log('email sent successfully')
+                    console.log(newUser);
+                    const token = jwt.sign(
+                      {
+                        userType: newUser.userType,
+                        userId: newUser._id,
+                        email: newUser.email,
+                        name: newUser.name,
+                        isEmailVerified: newUser.isEmailVerified,
+                      },
+                      process.env.JWT_SECRET,
+                      {
+                        expiresIn: "1d",
+                      }
+                    );
+                    Student.findById(newUser._id).then((result7) => {
+                      result7.token = token;
+                      result7
+                        .save()
+                        .then((user) => {
+                          console.log(user);
+                          done(null, user);
+                        })
+                        .catch((err) => {
+                          console.log(err);
+                        });
                     });
-                });
+                  })
+                  .catch((err) => {
+                    console.log(err);
+                  });
               })
               .catch((err) => {
                 console.log(err);
