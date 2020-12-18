dd80292efbde0ec83e90546042ff50e61ca97aa8
jugaldb <jugalbhatt3@gmail.com>
2020-12-18 15:56:10 +0530

    redirect url for local

--- api/routes/auth.routes.js
+++ api/routes/auth.routes.js
@@ -2,38 +2,33 @@ const express = require("express");
 const passport = require("passport");
 const cookieParser = require("cookie-parser");
 
-
 const router = express.Router();
 
 router.use(cookieParser());
 
-
 router.get(
-	"/google",
-	passport.authenticate("google", {
-		hd: 'vitstudent.ac.in',
-		scope: ["profile", "email"],
-		prompt: 'select_account'
-	})
+  "/google",
+  passport.authenticate("google", {
+    hd: "vitstudent.ac.in",
+    scope: ["profile", "email"],
+    prompt: "select_account",
+  })
 );
 
 ///Callback route for google to redirect
 router.get(
-	"/google/redirect",
-	passport.authenticate("google"),
-	(req, res, next) =>{
-		const x = req.user;
-		var token = encodeURIComponent(req.user.token);
-		var name = encodeURIComponent(req.user.name);
-		res.redirect(
-			303,
-			// "https://cet.codechefvit.com/?name=" + name + "&token=" + token
-			"http://localhost:3000/student/oauth/?token=" + token
-		);
-		
-	}
+  "/google/redirect",
+  passport.authenticate("google"),
+  (req, res, next) => {
+    const x = req.user;
+    var token = encodeURIComponent(req.user.token);
+    var name = encodeURIComponent(req.user.name);
+    res.redirect(
+      303,
+      // "https://cet.codechefvit.com/?name=" + name + "&token=" + token
+      "http://localhost:3000/student/oauth/" + token + "/"
+    );
+  }
 );
-
-
 
 module.exports = router;
