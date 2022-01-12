2bec81fcbd62c8f272756b7e2994395efbf88036
jugaldb <jugalbhatt3@gmail.com>
2022-01-12 11:37:52 +0530

    check

--- api/controllers/test.controllers.js
+++ api/controllers/test.controllers.js
@@ -333,6 +333,7 @@ const attempt = async (req, res, next) => {
       var rn = Date.now()
       var d = new Date(rn);
       d.toLocaleString('en-US', { timeZone: 'Asia/Kolkata' });
+      test.scheduledForDate.toLocaleString('en-US', { timeZone: 'Asia/Kolkata' });
       console.log(test.scheduledForDate)
       console.log(d)
       console.log(rn)
