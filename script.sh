ca875dad44ad57aa38835cb3cc94c3ed0e8645eb
N0v0cain3 <nousernameidea0709@gmail.com>
2022-01-28 10:59:27 +0530

    Caddyfile

--- script.sh
+++ script.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+sudo apt update
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install 16
 nvm use 16
 npm install -g npm
 npm install -g pm2
+
+sudo apt install nginx
+
 
 # git clone https://N0v0cain3:{token}@github.com/CodeChefVIT/cetBackend.git
 # cd cetBackend/
