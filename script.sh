af9df470c576e97662821e6e220cf455a362aaa7
Shivam Mehta <50650500+N0v0cain3@users.noreply.github.com>
2022-01-31 10:40:22 +0530

    caddy

--- script.sh
+++ script.sh
@@ -12,6 +12,7 @@ curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo tee /et
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
 sudo apt update
 sudo apt install caddy
+sudo setcap CAP_NET_BIND_SERVICE=+eip $(which caddy)
 
 
 
